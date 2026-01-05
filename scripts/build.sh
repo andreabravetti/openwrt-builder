e01734356bc120f2cc948d179773ff0f9dadfd4b
Andrea Bravetti <a.bravetti@nephila.digital>
2026-01-05 13:29:12 +0100

    fix: defconfig and download before menuconfig to avoid configuration corruption

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,14 +7,25 @@ cd /openwrt || {
     exit 1
 }
 
-# Personalize for your device:
-wget https://downloads.openwrt.org/releases/24.10.5/targets/mediatek/filogic/config.buildinfo -O .config
-
+# Update feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
+# Personalize for your device
+wget https://downloads.openwrt.org/releases/24.10.5/targets/mediatek/filogic/config.buildinfo -O .config
+
+# Patch buggy hardware
+wget https://raw.githubusercontent.com/Gilly1970/BPI-R4_Openwrt_Snapshot_Build/refs/heads/main/openwrt-patches/9999-new-tx-power-eeprom-0s.patch -O package/kernel/mt76/patches/9999-new-tx-power-eeprom-0s.patch
+
+# Prepare
+make defconfig download
+
+# Configure
 make menuconfig
 
-make defconfig download clean world
+# Build
+make -j8 clean world
+
+ls -l bin/targets/mediatek/filogic
 
 echo "OpenWrt built!"
