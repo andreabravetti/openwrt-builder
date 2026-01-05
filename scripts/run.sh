7db31f104dc0fc55ba1354a3f99efe1ab4d5ccf7
Andrea Bravetti <a.bravetti@nephila.digital>
2026-01-06 00:50:34 +0100

    fix: Add the openwrt user in docker to make git happy

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,5 +12,13 @@ make distclean
 BECOME_UID=$(stat -c '%u' /openwrt)
 BECOME_GID=$(stat -c '%g' /openwrt)
 
+# Setup the user
+sed -i "/^openwrt:x:.*$/d" /etc/group
+echo "openwrt:x:$BECOME_GID:" | tee -a /etc/group
+sed -i "/^openwrt:x:.*$/d" /etc/passwd
+echo "openwrt:x:$BECOME_UID:$BECOME_GID:openwrt:/home/openwrt:/bin/bash" | tee -a /etc/passwd
+mkdir -p /home/openwrt
+chown -R openwrt:openwrt /home/openwrt
+
 # shellcheck disable=SC2086
 sudo -u \#$BECOME_UID -g \#$BECOME_GID bash /build.sh
