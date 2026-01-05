351f0f360e09bd362a6844cc62532ac77b1e4c29
Andrea Bravetti <a.bravetti@nephila.digital>
2026-01-05 11:23:09 +0100

    feat: test for OpenWrt source

--- 02_run.sh
+++ 02_run.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 BUILDER_IMAGE=builder-debian12
+SOURCE_PATH=../openwrt
+
+[ -d $SOURCE_PATH ] || {
+    echo "OpenWrt source not found in $SOURCE_PATH."
+    echo "Clone OpenWrt somewhere and personalize this script."
+    exit 1
+}
 
 docker ps -aq --filter="name=$BUILDER_IMAGE" \
 | xargs -r docker stop | xargs -r docker rm
 
 docker run -d --name $BUILDER_IMAGE --rm \
-       -v ../openwrt:/openwrt \
+       -v $SOURCE_PATH:/openwrt \
        -it $BUILDER_IMAGE:latest
 
 sleep 1
